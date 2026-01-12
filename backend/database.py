26cf0d8e0c4be82508cf4a46a1a0487f75bd70f5
ANURAG PANDEY <anuragpandey1rkt@gmail.com>
2026-01-12 14:25:20 +0530

    adding database_url

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "DATABASE_URL"
+DATABASE_URL = "postgresql://study_buddy_db_gpua_user:SlDUsn8jgdbnUbGYAMkvqAB6IeV9U9ts@dpg-d5ia0kv5r7bs73btc0tg-a/study_buddy_db_gpua"
+
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
